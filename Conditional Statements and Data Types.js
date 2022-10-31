99f00517274f7ecbd86acbae50c9283fc74ea71a
Munira Tokhirova <muniro4ka555@gmail.com>
2022-10-31 12:29:10 +0500

    Added files

---
+++ Conditional Statements and Data Types.js
@@ -0,0 +1,41 @@
+
+
+// Perform addition of various types (string + boolean, string + number, number + boolean)
+let str = 'Cat';
+let nmb = 12;
+let bool = false;
+console.log(str + bool);
+console.log(str + nmb);
+console.log(nmb + bool);
+
+//Perform multiplication of various types (string * boolean, string * number, number * boolean)
+
+console.log(str * bool);
+console.log(str * nmb);
+console.log(nmb * bool);
+
+//Perform multiplication of various types (string * boolean, string * number, number * boolean)
+
+console.log(str + bool);
+console.log(str + nmb);
+console.log(nmb + bool);
+
+//Divide different types (string / boolean, string / number, number / Boolean)
+
+console.log(str / bool);
+console.log(str / nmb);
+console.log(nmb / bool);
+
+
+//Perform explicit conversion (number, string, boolean)
+
+const quantity = "12";
+console.log(Number(quantity));
+
+let result;
+result = String(324);
+console.log(result);
+
+let booltrue = true;
+console.log(Number(booltrue));
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # JS-tasks
 JS tasks
+
+
+# Conditional statements and Data types Practical task 
+
+1. Perform addition of various types (string + boolean, string + number, number + boolean)
+2. Perform multiplication of various types (string * boolean, string * number, number * boolean)
+3. Divide different types (string / boolean, string / number, number / Boolean)
+4. Perform explicit conversion (number, string, boolean)
