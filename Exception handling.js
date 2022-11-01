e05fdfb362dcb582ed515ed0438f4d776ecb145d
Munira Tokhirova <muniro4ka555@gmail.com>
2022-11-01 14:43:40 +0500

    Added file (Exception handling)

---
+++ Exception handling.js
@@ -0,0 +1,10 @@
+// In a try catch construction, wrap the code: console.log (a), let a = 3. 
+//And display an error - ‘let must be declared’ before use. When running 1/0, the error 'cannot be divided by zero'
+
+try {
+    console.log(a);
+    let a = 3;
+}
+catch (err) {
+    console.error(err)
+}
